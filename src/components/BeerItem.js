c182aad3c9139e5d0e051039fe0701e6efdd90c3
PatrikBeichner <patrik.beichner@gmail.com>
2021-03-25 12:01:54 -0400

    added style and labels and regions

--- src/components/BeerItem.js
+++ src/components/BeerItem.js
@@ -1,30 +1,30 @@
 // import React from 'react';
 
-// function RenderBeer({ beer }) {
+// function RenderBeer({ item }) {
 //   return (
 //     <div class="card mb-4">
 //       <div class="embed-responsive">
 //         <img class="card-img-top embed-responsive-item" src={beer.labels.contentAwareMedium} />
 //       </div>
 //       <div class="card-block">
-//         <h4 class="card-title">{beer.name}</h4>
-//         <p class="card-text">{beer.style.description}</p>
+//         <h4 class="card-title">{item.name}</h4>
+//         <p class="card-text">{item.style.description}</p>
 //       </div>
 //     </div>
 //   );
 // }
 
 // function BeerItem(props) {
-//   const list = props.data.map((beer) => {
+//   const list = props.data.map((item) => {
 //     return (
-//       <div key={beer.id}>
-//         <RenderBeer beer={beer} />
+//       <div key={item.id}>
+//         <RenderBeer item={item} />
 //       </div>
 //     );
 //   });
 //   console.log('heres props:' + props.data);
-//   // return <div>{list}</div>;
-//   return <div></div>;
+//   return <div>{list}</div>;
+//   // return <div></div>;
 // }
 
 // export default BeerItem;
@@ -32,13 +32,54 @@
 import React from 'react';
 
 const BeerItem = ({ item }) => {
-  console.log('heres props:' + item);
-  return (
-    <div>
-      <p>{item.name}</p>
-      {/* <p>{item.style.name}</p> */}
-    </div>
+  // console.log('heres props:' + item);
+  // console.log(item.style)
+  console.log(item.name)
+  console.log(item.labels)
+
+
+  if(item.style){
+
+    if(item.labels) {
+
+    console.log('heres label: ' + item.labels.icon)
+    return (
+    // <div>
+    //   <p>{item.name}</p>
+    //   <p>{item.style.shortName}</p>
+    // </div>
+    <div className="card mb-4">
+       <div className="embed-responsive">
+         <img src={item.labels.medium} />
+     </div>
+     <div className="card-block">
+         <h4 className="card-title">{item.name}</h4>
+         <p className="card-text">{item.style.shortName}</p>
+       </div>
+     </div>
   );
+    } else {
+      return (
+        <div class="card mb-4">
+       <div class="embed-responsive">
+         {/* <img class="card-img-top embed-responsive-item" src={item.labels.icon} /> */}
+     </div>
+     <div class="card-block">
+         <h4 class="card-title">{item.name}</h4>
+         <p class="card-text">{item.style.shortName}</p>
+       </div>
+     </div>
+      )
+
+    }
+  } else {
+    return (
+      <div>
+      <p>{item.name}</p>
+      <p>style undefined</p>
+      </div>
+    )
+  }
 };
 
 export default BeerItem;
