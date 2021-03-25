a1abb6aaa976618d3b78ed5b8a9eaa7a4335fb42
PatrikBeichner <patrik.beichner@gmail.com>
2021-03-25 10:31:43 -0400

    backend tie in

--- src/components/BeerItem.js
+++ src/components/BeerItem.js
@@ -1,9 +1,42 @@
-import React from "react";
+// import React from 'react';
+
+// function RenderBeer({ beer }) {
+//   return (
+//     <div class="card mb-4">
+//       <div class="embed-responsive">
+//         <img class="card-img-top embed-responsive-item" src={beer.labels.contentAwareMedium} />
+//       </div>
+//       <div class="card-block">
+//         <h4 class="card-title">{beer.name}</h4>
+//         <p class="card-text">{beer.style.description}</p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// function BeerItem(props) {
+//   const list = props.data.map((beer) => {
+//     return (
+//       <div key={beer.id}>
+//         <RenderBeer beer={beer} />
+//       </div>
+//     );
+//   });
+//   console.log('heres props:' + props.data);
+//   // return <div>{list}</div>;
+//   return <div></div>;
+// }
+
+// export default BeerItem;
+
+import React from 'react';
 
 const BeerItem = ({ item }) => {
+  console.log('heres props:' + item);
   return (
     <div>
       <p>{item.name}</p>
+      {/* <p>{item.style.name}</p> */}
     </div>
   );
 };
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import Navigation from "./NavigationComponent";
-import Home from "./HomeComponent";
-import Beer from "./BeerComponent";
-import { Switch, Route, Redirect } from "react-router-dom";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import Navigation from './NavigationComponent';
+import Home from './HomeComponent';
+import Beer from './BeerComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 const Main = () => {
   const [items, setItems] = useState([]);
@@ -12,11 +12,11 @@ const Main = () => {
   useEffect(() => {
     const fetchItems = async () => {
       const result = await axios(
-        `http://api.brewerydb.com/v2/beers/?key=3f3f3ba75535847b0038530a898a42a1`
+        `http://localhost:3000/brewery/beers`
         //`https://rickandmortyapi.com/api/character`
       );
       console.log(result.data);
-      setItems(result.data);
+      setItems(result.data.data);
       setIsLoading(false);
     };
     fetchItems();
@@ -27,11 +27,7 @@ const Main = () => {
       <Navigation />
       <Switch>
         <Route path="/home" component={Home} />
-        <Route
-          exact
-          path="/beer"
-          render={() => <Beer isLoading={isLoading} items={items} />}
-        />
+        <Route exact path="/beer" render={() => <Beer isLoading={isLoading} items={items} />} />
         <Redirect to="/home" />
       </Switch>
     </div>
